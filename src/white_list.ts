5b37e63ef372d0292c62fa97919407935fe0c2ca
yilong <yilong@yunjingtech.cn>
2025-10-22 10:19:53 +0800

    feat: update whitelist2

--- src/white_list.ts
+++ src/white_list.ts
@@ -4,5 +4,6 @@ export default [
 	// "https://glink25.github.io",
 	// "https://oncent.github.io",
 	// "https://cent.linkai.work",
+	"https://cent-6de.pages.dev",
 	"https://cent-backend.yiloooooooong.workers.dev"
 ];
