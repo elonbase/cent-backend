57bffe2a2a7c7a38fd383e03938ab916a46c22ab
yilong <yilong@yunjingtech.cn>
2025-10-22 09:27:50 +0800

    feat: deploy

--- src/white_list.ts
+++ src/white_list.ts
@@ -1,8 +1,8 @@
 export default [
 	"http://localhost", // localhost
 	"http://127.0.0.1", // localhost
-	"https://glink25.github.io",
-	"https://oncent.github.io",
-	"https://cent.linkai.work",
-	"https://cent.link-ai.workers.dev"
+	// "https://glink25.github.io",
+	// "https://oncent.github.io",
+	// "https://cent.linkai.work",
+	"https://cent.yiloooooooong.workers.dev"
 ];
